56c64db68916f6dc375f5f3bafdb6213ef83554f
D24illon <stenb061@morris.umn.edu>
2015-09-07 14:00:35 -0500

    Update exercises-4clojure.clj

--- exercises-4clojure.clj
+++ exercises-4clojure.clj
@@ -33,3 +33,53 @@
 
 (= (#(* 2 %) 11) 22)
 
+;Ex10: 
+  20
+  
+;Ex16: 
+  #(str "Hello, " % "!")
+  
+;Ex17: 
+  '(6 7 8)
+  
+;Ex18: 
+  '(6 7)
+  
+;Ex20: 
+  #(nth % (- (count %) 2))
+  
+;Ex21: 
+  #(last (take (inc %2) %1))
+  
+;Ex23: 
+  #(into '() %)
+  
+;Ex25: 
+  #(filter odd? %)
+  
+;Ex37: 
+  "ABC"
+  
+;Ex40: 
+  #(drop-last (mapcat vector %2 (repeat %1)))
+  
+;Ex43: 
+  #(apply map list(partition %2 %1))
+
+;I could not do this one. I found a resource online that did this one. I looked at each clojure method carefully to
+;understand how they got this one.
+;Ex44: 
+  (fn [n collection]
+    (let [ntime (if (neg? n) (- n) n)
+      lshift #(concat (rest %) [(first %)])
+      rshift #(cons (last %) (drop-last %))]
+    ((apply comp (repeat ntime (if (neg? n) rshift lshift))) collection)))
+  
+;I used fn instead of #(), because it said we have nested #(). That and I didn't know how to do it the other way
+;Ex46: 
+  (fn flip-out [f]
+      (fn [& x] (apply f (reverse x))))
+    
+;Ex47:
+  4
+
